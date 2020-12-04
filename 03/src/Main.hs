166484d56981f6a22481c31beae0edeaab54671a
Lukas Stevens <mail@lukas-stevens.de>
2020-12-04 21:52:54 +0100

    Golfing 03

--- 03/src/Main.hs
+++ 03/src/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
-countTrees :: Int -> Int -> [String] -> Int
-countTrees r d = countTrees'
+countTrees :: (Int, Int) -> [String] -> Int
+countTrees (r, d) = countTrees'
   where
     countTrees' [] = 0
     countTrees' (xs : xss) = fromEnum (head xs == '#') + countTrees' (drop (d - 1) $ drop r <$> xss)
@@ -9,4 +9,4 @@ countTrees r d = countTrees'
 main :: IO ()
 main = do
   forest <- map cycle . lines <$> readFile "input.txt"
-  print $ product [countTrees r d forest | (r, d) <- [(1,1), (3,1), (5,1), (7,1), (1,2)]]
+  print $ product $ flip countTrees forest <$> (1,2):zip [1,3,5,7] (repeat 1)
