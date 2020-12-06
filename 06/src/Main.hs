f91713152ab7830490448c592b00b490d39c792a
Lukas Stevens <mail@lukas-stevens.de>
2020-12-06 11:30:11 +0100

    Add 06

---
+++ 06/src/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Data.List
+import Data.List.Split
+
+main :: IO ()
+main = do
+  answers <- splitWhen null . map nub . lines <$> readFile "input.txt"
+  print $ sum $ map (length . foldl intersect ['a'..'z']) answers
+
