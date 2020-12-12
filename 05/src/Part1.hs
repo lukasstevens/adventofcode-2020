09b03dd6f9558a2cf82bd2aa79343a14d2b72d26
Lukas Stevens <mail@lukas-stevens.de>
2020-12-12 20:10:48 +0100

    Beautify 05

--- 05/src/Part1.hs
+++ 05/src/Part1.hs
@@ -3,20 +3,12 @@ module Part1 where
 import Data.List
 import System.Environment
 
-search :: Char -> Char -> String -> Int -> Int -> Int
-search l h = search'
-  where
-    search' [] mi _ = mi
-    search' (x : xs) mi ma 
-      | x == l = search' xs mi mid 
-      | x == h = search' xs (mid + 1) ma 
-      | otherwise = undefined
-      where
-        mid = mi + ((ma - mi) `div` 2)
+fromBinary :: [Bool] -> Int
+fromBinary = foldl' (\acc x -> 2 * acc + fromEnum x) 0
 
 main :: IO ()
 main = do
   inputFile <- head <$> getArgs
   input <- map (splitAt 7) . lines <$> readFile inputFile 
-  print $ maximum [search 'F' 'B' r 0 127 * 8 + search 'L' 'R' c 0 7  | (r, c) <- input]
+  print $ maximum [fromBinary (map (== 'B') r) * 8 + fromBinary (map (== 'R') c)  | (r, c) <- input]
 
