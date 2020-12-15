313df9419b77d7f35d3fd4fecf855f808e770a40
Lukas Stevens <mail@lukas-stevens.de>
2020-12-15 22:40:59 +0100

    15: use IntMap

--- 15/src/Part2.hs
+++ 15/src/Part2.hs
@@ -1,11 +1,10 @@
 module Part2 where
 
-import qualified Data.Map.Strict as M
+import qualified Data.IntMap.Strict as M
 import qualified Data.Sequence as S
 import System.Environment
 
-
-turns :: M.Map Int Int -> S.Seq Int -> [Int] 
+turns :: M.IntMap Int -> S.Seq Int -> [Int] 
 turns m nums@(_ S.:|> prev) =
   case M.lookup prev m of
     Nothing -> 0 : turns m' (nums S.|> 0)
@@ -14,8 +13,6 @@ turns m nums@(_ S.:|> prev) =
        in idxDiff : turns m' (nums S.|> idxDiff) 
   where
     m' = M.insert prev (S.length nums) m
-                      
-                    
                                             
 main :: IO ()
 main = do
