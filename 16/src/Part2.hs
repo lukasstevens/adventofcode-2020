7fd600e9476e40cf48f9b51e70da0dd46bb7620c
Lukas Stevens <mail@lukas-stevens.de>
2020-12-16 21:12:07 +0100

    16: more bifunctor

--- 16/src/Part2.hs
+++ 16/src/Part2.hs
@@ -1,6 +1,6 @@
 module Part2 where
 
-import Data.Bifunctor (second)
+import Data.Bifunctor (bimap, second)
 import Data.List
 import Data.List.Split
 import Data.Ix
@@ -9,7 +9,7 @@ import System.Environment
 toSndRange :: [String] -> (String, [(Int, Int)])
 toSndRange [field, rs] =
   case wordsBy (`elem` "- ") rs of
-    [f1, t1, _, f2, t2] -> (field, [(read f1, read t1), (read f2, read t2)])
+    [f1, t1, _, f2, t2] -> (field, bimap read read <$> [(f1, t1), (f2, t2)])
 
 determineFields :: [(Int, [String])] -> [(Int, String)]
 determineFields [] = []
@@ -28,8 +28,8 @@ main = do
   let isValid t = and [ any (`inRange` n) (concatMap snd fields) | n <- t ]
   let nearbyTickets = filter isValid . map (map read . splitOn ",") $ tail $ input !! 2
 
-  let possFieldsTicket n = [ f | (f, rs) <- fields, any (`inRange` n) rs ]
-  let possFieldsTickets = map possFieldsTicket <$> nearbyTickets
+  let possFieldsVal n = [ f | (f, rs) <- fields, any (`inRange` n) rs ]
+  let possFieldsTickets = map possFieldsVal <$> nearbyTickets
   let possFields = foldr1 intersect <$> transpose possFieldsTickets
   let detFields = map snd $ sortOn fst $ determineFields $ zip [0..] possFields
 
